da02d7841a27fb62436809d8868844491b9227c4
DominikIlski <dominikk.ilski@gmail.com>
2022-12-07 15:55:30 +0100

    srcipt update 4

--- .github/workflows/src/version_read.sh
+++ .github/workflows/src/version_read.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-VERSION=$(cat ./VERSION)
-echo "$VERSION"
+if (VERSION=$(cat ./VERSION)) then
+    echo "no version fille assuming v0.0.0"
+    VERSION=0.0.0
+fi
 git fetch origin
+echo "$BASE_REF"
+echo "gh ref $GITHUB_HEAD_REF"
+echo "$HEAD_REF"
 echo Master "$(git show origin/test_master:VERSION)"
-git log --graph --pretty=oneline --abbrev-commit
 echo "VERSION=$VERSION">> "$GITHUB_ENV"
 if [ -z "$VERSION" ]
 then
