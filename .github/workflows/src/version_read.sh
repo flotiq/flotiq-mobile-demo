693aed2b7ce02e09158d0e56bbe226e4e4d52aa5
DominikIlski <dominikk.ilski@gmail.com>
2022-12-07 16:52:58 +0100

    script update 5

--- .github/workflows/src/version_read.sh
+++ .github/workflows/src/version_read.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
-if (VERSION=$(cat ./VERSION)) then
-    echo "no version fille assuming v0.0.0"
-    VERSION=0.0.0
-fi
 git fetch origin
-echo "$BASE_REF"
-echo "gh ref $GITHUB_HEAD_REF"
-echo "$HEAD_REF"
-echo Master "$(git show origin/test_master:VERSION)"
-echo "VERSION=$VERSION">> "$GITHUB_ENV"
-if [ -z "$VERSION" ]
-then
-echo "There is no version available"
-exit 1
+npx -q semver-compare-cli --help > /dev/null || echo ""
+(apt update && apt install -y jq) > /dev/null
+if [ -f VERSION ]; then
+    CURRENT_VERSION=$(cat ./VERSION)
 else
-echo "VERSION=$VERSION" >> "$GITHUB_OUTPUT"
+    echo "No VERSION exists, assuming v0.0.0"
+    CURRENT_VERSION="0.0.0"
+fi
+if ! OLD_VERSION=$(git show "$GITHUB_TARGET_REF":VERSION); then
+    echo "No remote VERSION exists, assuming v0.0.0"
+    REMOTE_VERSION="0.0.0"
+fi
+echo "REMOTE_VERSION=$REMOTE_VERSION"
+echo "OLD_VERSION=$OLD_VERSION"
+echo "CURRENT_VERSION=$CURRENT_VERSION"
+echo "VERSION=$VERSION">> "$GITHUB_ENV"
+
+if npx semver-compare-cli $CURRENT_VERSION gt "$OLD_VERSION";
+    then echo_green VERSION increased on "$GITHUB_BASE_REF"
+    else echo_red VERSION needs to increase on "$GITHUB_BASE_REF"; FAILED=true
+fi
+
+if [ -n "$FAILED" ]; then
+    echo "Some checks for versioning failed"
+    exit 1;
 fi
